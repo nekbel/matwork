fc9d100ae14ebb4d3933dc3c928402eafdfee0c4
Nek <nek.belmezos@gmail.com>
2022-10-05 02:33:33 +0300

    organisms script

---
+++ scripts/orgUCSC.sh
@@ -0,0 +1,34 @@
+if [ "$#" -ne 2 ]; then
+echo "Usage: sh <scriptname>.sh <orgname> <input>"
+echo "       <orgname> organism name (USCS version)"
+echo "       <input> .txt file containing prefexes of files"
+echo ""
+echo "Other Options: use sh -x so every command is printed"
+exit 1
+fi
+
+org=$1
+orgfull=''
+input=$2
+
+if [[ "$org" == *'mm'* ]]; then
+    orgfull='Mus_musculus'
+elif [[ "$org" == *'rn'* ]]; then
+    orgfull='Rattus_norvegicus'
+elif [[ "$org" == *'hg'* ]]; then
+    orgfull='Homo_sapiens'
+elif [[ "$org" == *'ce'* ]]; then
+    orgfull='Caenorhabditis_elegans'
+elif [[ "$org" == *'sacCer'* ]]; then
+    orgfull='Saccharomyces_cerevisiae'
+elif [[ "$org" == *'dm'* ]]; then
+    orgfull='Drosophila_melanogaster'
+elif [[ "$org" == *'danRer'* ]]; then
+    orgfull='Danio_rerio'
+else
+    echo 'ERROR: wrong organism name provided (USCS version)'
+    exit 1
+fi
+
+echo "/home/myrto/work/refs/${orgfull}/UCSC/${org}/"
+exit 0
