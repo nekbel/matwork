f48b0869acbe6f08b9a80af4aa6def3c080497b3
Nektarios Belmezos <bio3089@csd.uoc.gr>
2022-10-07 17:20:14 +0300

    sh update

--- scripts/notepad.sh
+++ scripts/notepad.sh
@@ -50,77 +50,6 @@ wget -r -nH -nd -np -R "*_random.fa.gz, chrUn_*" https://hgdownload.soe.ucsc.edu
 
 
 ##########################
-# LAVIGNE RNA PIPELINE LOOP
-##########################
-FILE="lane1RNA2MIRANTAWT2"
-
-# alignment
-hisat2 -p24 \
--x $REFERENCE_GENOME \
--1 fastq/AAANCYHHV_MIRANTA_PV_0722_22s003034-1-1_Fett_lane1RNA2MIRANTAWT2_1_sequence.txt \
--2 fastq/AAANCYHHV_MIRANTA_PV_0722_22s003034-1-1_Fett_lane1RNA2MIRANTAWT2_2_sequence.txt \
--S sam/$FILE.sam \
---rna-strandness FR \
---score-min L,0,-0.5 \
--k 3
-
-samtools sort -@ 32 -o sam-bam/$FILE.bam sam/$FILE.sam
-rm sam/$FILE.sam
-samtools index sam-bam/$FILE.bam
-
-mv sam-bam/$FILE.bam /data/www/Lavigne/tracks/miranta_bams/
-mv sam-bam/$FILE.bam.bai /data/www/Lavigne/tracks/miranta_bams/
-
-echo 'track type=bam pairEndsByName=. name=lane1RNA2MIRANTAWT2_bam description=lane1RNA2MIRANTAWT2 visibility=pack bigDataUrl=http://139.91.171.49/Lavigne/tracks/miranta_bams/lane1RNA2MIRANTAWT2.bam'
-
-# WT & KO MERGE 
-samtools merge -@ 32 /data/www/Lavigne/tracks/miranta_bams/RNA_MIRANTA_WT.merged.bam /data/www/Lavigne/tracks/miranta_bams/*RNA*WT*.bam
-samtools sort -@ 32 -o /data/www/Lavigne/tracks/miranta_bams/RNA_MIRANTA_WT.merged.sorted.bam /data/www/Lavigne/tracks/miranta_bams/RNA_MIRANTA_WT.merged.bam
-samtools index /data/www/Lavigne/tracks/miranta_bams/RNA_MIRANTA_WT.merged.sorted.bam
-
-# WT & KO BW
-bamCoverage \
---binSize 10 \
---normalizeUsing RPKM \
---maxFragmentLength 0 \
--b RNA_MIRANTA_WT.sorted.merged.bam \
--o RNA_MIRANTA_WT.sorted.merged.bw \
--p 24
-
-echo 'track type=bigWig name=RNA_MIRANTA_WT.merged_bw description=RNA_MIRANTA_WT.merged_bw visibility=full maxHeightPixels=64:64:11 color=50,50,150 bigDataUrl=http://139.91.171.49/Lavigne/tracks/miranta_bams/RNA_MIRANTA_WT.merged.sorted.bw'
-
-# Process others
-bamCoverage \
---binSize 10 \
---normalizeUsingRPKM \
---maxFragmentLength 0 \
--b /data/www/Lavigne/tracks/miranta_bams/lane1RNA1MIRANTAWT1.bam \
--o /data/www/Lavigne/tracks/miranta_bams/lane1RNA1MIRANTAWT1.bw \
--p 24
-echo 'track type=bigWig name=lane1RNA1MIRANTAWT1_bw description=lane1RNA1MIRANTAWT1_fwd visibility=full bigDataUrl=http://139.91.171.49/Lavigne/tracks/miranta_bams/lane1RNA1MIRANTAWT1.bw'
-
-bamCoverage \
---binSize 10 \
---normalizeUsingRPKM \
---maxFragmentLength 0 \
---filterRNAstrand=forward \
--b /data/www/Lavigne/tracks/miranta_bams/lane1RNA1MIRANTAWT1.bam \
--o /data/www/Lavigne/tracks/miranta_bams/lane1RNA1MIRANTAWT1_fwd.bw \
--p 24
-echo 'track type=bigWig name=lane1RNA1MIRANTAWT1_fwd_bw description=lane1RNA1MIRANTAWT1_fwd visibility=full bigDataUrl=http://139.91.171.49/Lavigne/tracks/miranta_bams/lane1RNA1MIRANTAWT1_fwd.bw'
-
-bamCoverage \
---binSize 10 \
---normalizeUsingRPKM \
---maxFragmentLength 0 \
---filterRNAstrand=reverse \
--b /data/www/Lavigne/tracks/miranta_bams/lane1RNA1MIRANTAWT1.bam \
--o /data/www/Lavigne/tracks/miranta_bams/lane1RNA1MIRANTAWT1_rev.bw \
--p 24
-echo 'track type=bigWig name=lane1RNA1MIRANTAWT1_rev_bw description=lane1RNA1MIRANTAWT1_rev visibility=full bigDataUrl=http://139.91.171.49/Lavigne/tracks/miranta_bams/lane1RNA1MIRANTAWT1_rev.bw'
-
-
-##########################
 # disk used
 ##########################
 du -hs * | sort -h
@@ -190,3 +119,82 @@ trim_galore -o ./trimmed_fqs -q 20 ./fqs/SRR12080416; trim_galore -o ./trimmed_f
 
 bowtie2 --local -X 3000 -p 25 -x /home/myrto/work/refs/Homo_sapiens/UCSC/hg19/Sequence/Bowtie2Index/genome -U ./trimmed_fqs/SRR12080416_trimmed.fq | samtools sort -O BAM - > ./bowtie/SRR12080416.sorted.bam
 bowtie2 --local -X 3000 -p 25 -x /home/myrto/work/refs/Homo_sapiens/UCSC/hg19/Sequence/Bowtie2Index/genome -U ./trimmed_fqs/SRR12080417_trimmed.fq | samtools sort -O BAM - > ./bowtie/SRR12080417.sorted.bam
+
+
+##########################
+# track comp hub 
+##########################
+track type=bigWig name=RNA_MIRANTA_KO.sorted.merged description=RNA_MIRANTA_KO.sorted.merged visibility=full bigDataUrl=http://139.91.171.49/Lavigne/Nektarios/tracks/miranta_PV/RNA_MIRANTA_KO.sorted.merged.bw color=0,255,0
+track type=bigWig name=RNA_MIRANTA_WT.sorted.merged description=RNA_MIRANTA_WT.sorted.merged visibility=full bigDataUrl=http://139.91.171.49/Lavigne/Nektarios/tracks/miranta_PV/RNA_MIRANTA_WT.sorted.merged.bw color=255,0,0
+
+hub MirantaHub
+shortLabel Miranta Hub
+longLabel RNA and ATAC runs on the Miranta files
+useOneFile on
+email nek.belmezos@gmail.com
+
+genome mm10
+
+track ComparisonKOWT
+type bigWig
+container multiWig
+visibility full
+aggregate transparentOverlay
+showSubtrackColorOnUi on
+maxHeightPixels 500:100:8
+
+        track KO
+        type bigWig
+        bigDataUrl http://139.91.171.49/Lavigne/Nektarios/tracks/miranta_PV/RNA_MIRANTA_KO.sorted.merged.bw
+        parent ComparisonKOWT
+        color 70,250,150
+
+        track WT
+        type bigWig
+        bigDataUrl http://139.91.171.49/Lavigne/Nektarios/tracks/miranta_PV/RNA_MIRANTA_WT.sorted.merged.bw
+        parent ComparisonKOWT
+        color 207,21,62
+
+
+##########################
+# Hub track example
+##########################
+track multiWig1
+type bigWig
+container multiWig
+shortLabel Ex. multiWig container
+longLabel This multiWig overlay track graphs points from three bigWig files.
+visibility full
+aggregate transparentOverlay
+showSubtrackColorOnUi on
+maxHeightPixels 500:100:8
+viewLimits 1:20
+priority 1
+html examplePage
+
+				track wig1
+				bigDataUrl http://hgdownload.soe.ucsc.edu/goldenPath/hg19/encodeDCC/wgEncodeCshlLongRnaSeq/wgEncodeCshlLongRnaSeqA549CellLongnonpolyaMinusRawSigRep1.bigWig
+				shortLabel Overlay bigWig1
+				longLabel This is an example bigWig1 displaying Raw Signal from the ENCODE RNA-seq CSHL track, graphing just points as default.
+				parent multiWig1
+				graphTypeDefault points
+				type bigWig
+				color 255,0,0
+
+				track wig2
+				bigDataUrl http://hgdownload.soe.ucsc.edu/goldenPath/hg19/encodeDCC/wgEncodeCshlLongRnaSeq/wgEncodeCshlLongRnaSeqA549CellLongnonpolyaPlusRawSigRep1.bigWig
+				shortLabel Overlay bigWig2
+				longLabel This is an example bigWig2 displaying Raw Signal from the ENCODE RNA-seq CSHL track, graphing just points as default.
+				graphTypeDefault points
+				parent multiWig1
+				type bigWig
+				color 0,255,0
+
+				track wig3
+				bigDataUrl http://hgdownload.soe.ucsc.edu/goldenPath/hg19/encodeDCC/wgEncodeCshlLongRnaSeq/wgEncodeCshlLongRnaSeqAg04450CellLongnonpolyaPlusRawSigRep1.bigWig
+				shortLabel Overlay bigWig3
+				longLabel This is an example bigWig3 displaying Raw Signal from the ENCODE RNA-seq CSHL track, graphing just points as default. 
+				graphTypeDefault points
+				parent multiWig1
+				type bigWig
+				color 95,158,160
--- scripts/notes.sh
+++ scripts/notes.sh
@@ -1,1 +1,58 @@
-echo hello world
+##########################
+# NGS analyses
+##########################
+
+traditional NGS analyses (e.g., QC, read mapping, variant calling, assembly etc.) .
+
+• Transcriptome gives us the complete set of transcripts
+It is in the form of a GTF (gene transfer format) file
+
+Transcriptome reference file
+
+Mapping results are only as good as the quality of the
+reference transcriptome
+
+• If one does not exist, it can be created using coordinates
+from a GTF file and the genome sequence file 
+
+salmon_in_serial
+Request 6 cores 
+Request 12 hours of runtime
+Request 8G of memory
+
+##########################
+# Bootstraping
+##########################
+Bootstraping is any test or metric that uses random sampling with replacement and falls under the broader 
+class of resampling methods. It uses sampling with replacement to estimate the sampling distribution for 
+the desired estimator. This approach is used to assess the reliability of sequence-based phylogeny.
+
+Bootstrapping is a statistical procedure that resamples a single dataset to create many simulated samples. 
+This process allows for the calculation of standard errors, confidence intervals, and hypothesis testing
+
+NOTE: --numBootstraps is necessary if performing isoform-level differential expression analysis with Sleuth, 
+but not for gene-level differential expression analysis. Due to the statistical procedure required to assign 
+reads to gene isoforms, in addition to the random processes underlying RNA-Seq, there will be technical 
+variability in the abundance estimates output from the pseudo-alignment tool [2, 3] for the isoform level 
+abundance estimates (not necessary for gene-level estimates). Therefore, we would need technical replicates 
+to distinguish technical variability from the biological variability for gene isoforms.
+
+The bootstraps estimate technical variation per gene by calculating the abundance estimates for all genes 
+using a different sub-sample of reads during each round of bootstrapping. The variation in the abundance 
+estimates output from each round of bootstrapping is used for the estimation of the technical variance for 
+each gene.
+
+##########################
+# alleles in refgen
+##########################
+GRCh38, contains information about alternative alleles for various locations on the genome, use the HISAT2 aligner
+as it is able to utilize this information during the alignment. 
+There is a version of GRCh38 that does not have alleles represented, which is the appropriate version to use with STAR. 
+
+
+##########################
+RNASEQ Metrics
+##########################
+number of raw reads
+percentage of reads aligned to genome
+percentage of reads associated with genes
